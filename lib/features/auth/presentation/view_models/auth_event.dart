f7838be3a9bb435d278a74d2d5c3834cc1b12ee1
sush911 <sshres381@gmail.com>
2025-06-19 16:06:01 +0545

    auth_event

---
+++ lib/features/auth/presentation/view_models/auth_event.dart
@@ -0,0 +1,25 @@
+part of 'auth_bloc.dart';
+
+abstract class AuthEvent extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class LoginRequested extends AuthEvent {
+  final String username;
+  final String password;
+
+  LoginRequested(this.username, this.password);
+
+  @override
+  List<Object?> get props => [username, password];
+}
+
+class RegisterRequested extends AuthEvent {
+  final User user;
+
+  RegisterRequested(this.user);
+
+  @override
+  List<Object?> get props => [user];
+}
