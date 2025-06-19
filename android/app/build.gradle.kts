75f1e5aafa99b54749f07eaebc4ce86002eb6acf
sush911 <sshres381@gmail.com>
2025-06-19 20:52:11 +0545

    fixed issues of sdk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,9 @@ plugins {
 android {
     namespace = "com.example.petforpat"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+
+    // Specify NDK version manually to match dependencies
+    ndkVersion = "27.0.12077973"  // Make sure to specify the correct version here
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -20,10 +22,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.petforpat"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,8 +31,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
