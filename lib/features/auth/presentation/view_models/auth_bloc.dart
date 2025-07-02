469526fdaa055d7cb3d3e92703151f81aa200027
sush911 <sshres381@gmail.com>
2025-07-02 22:06:12 +0545

    updating auth

--- lib/features/auth/presentation/view_models/auth_bloc.dart
+++ lib/features/auth/presentation/view_models/auth_bloc.dart
@@ -2,7 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:hive/hive.dart';
 import 'package:petforpat/app/shared_pref/shared_pref_service.dart';
-import 'package:petforpat/features/auth/domain/entities/register_user.dart';  // Import RegisterUser
+import 'package:petforpat/features/auth/domain/entities/register_user.dart';
 import '../../domain/entities/user.dart';
 import '../../domain/usecases/login_usecase.dart';
 import '../../domain/usecases/register_usecase.dart';
@@ -19,37 +19,30 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     required this.loginUseCase,
     required this.registerUseCase,
   }) : super(AuthInitial()) {
-    on<LoginRequested>((event, emit) async {
-      emit(AuthLoading());
-      try {
-        final user = await loginUseCase(event.username, event.password);
-        if (user != null) {
-          final box = Hive.box('profileInstalled');
-          await box.put('loginTime', DateTime.now().toIso8601String());
-          print('✅ Saved login time: ${box.get('loginTime')}');
-
-          await _sharedPrefService.saveLoginSession(
-            userId: user.id,
-            authToken: user.token,
-          );
-
-          emit(AuthSuccess(user));
-        } else {
-          emit(AuthFailure('Invalid username or password'));
-        }
-      } catch (e) {
-        emit(AuthFailure(e.toString()));
-      }
-    });
-
-    on<RegisterRequested>((event, emit) async {
-      emit(AuthLoading());
-      try {
-        await registerUseCase(event.user);  // event.user is RegisterUser
-        emit(RegisterSuccess());
-      } catch (e) {
-        emit(AuthFailure(e.toString()));
-      }
-    });
+    on<LoginRequested>(_onLoginRequested);
+    on<RegisterRequested>(_onRegisterRequested);
   }
-}
+
+  Future<void> _onLoginRequested(LoginRequested event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    try {
+      final User? user = await loginUseCase(event.username, event.password);
+      if (user != null) {
+        final box = Hive.box('profileInstalled');
+        await box.put('loginTime', DateTime.now().toIso8601String());
+        print('✅ Saved login time: ${box.get('loginTime')}');
+
+        await _sharedPrefService.saveLoginSession(
+          userId: user.id,
+          authToken: user.token,
+        );
+
+        emit(AuthSuccess(user));
+      } else {
+        emit(AuthFailure('Invalid username or password'));
+      }
+    } catch (e) {
+      emit(AuthFailure(e.toString()));
+    }
+  }
+
