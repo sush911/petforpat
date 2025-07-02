d64f3adad60c15bc6c2cc0bf0a9f9ef92e541ff9
sush911 <sshres381@gmail.com>
2025-07-02 21:20:15 +0545

    api client halfway

--- lib/core/network/api_client.dart
+++ lib/core/network/api_client.dart
@@ -1,10 +1,8 @@
-import 'package:dio/dio.dart';
-
 class ApiClient {
   final Dio _dio;
 
   ApiClient(this._dio) {
-    _dio.options.baseUrl = "http://<YOUR_API_HOST>:<PORT>/api";
+    _dio.options.baseUrl = "http://10.0.2.2:3000/api";
     _dio.options.connectTimeout = const Duration(seconds: 10);
     _dio.options.receiveTimeout = const Duration(seconds: 10);
   }
