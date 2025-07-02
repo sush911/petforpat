efb416cc74d81d19a0ebadd1a4dde2a170a376e3
sush911 <sshres381@gmail.com>
2025-07-02 22:07:50 +0545

    auth event

--- lib/features/auth/presentation/view_models/auth_event.dart
+++ lib/features/auth/presentation/view_models/auth_event.dart
@@ -9,16 +9,16 @@ class LoginRequested extends AuthEvent {
   final String username;
   final String password;
 
-  LoginRequested(this.username, this.password);
+  LoginRequested({required this.username, required this.password});
 
   @override
   List<Object?> get props => [username, password];
 }
 
 class RegisterRequested extends AuthEvent {
-  final RegisterUser user;  // <-- changed to RegisterUser
+  final RegisterUser user;
 
-  RegisterRequested(this.user);
+  RegisterRequested({required this.user});
 
   @override
   List<Object?> get props => [user];
