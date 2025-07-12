1b519f74461d556b412bcd486dfffd75da82e8d7
sush911 <sshres381@gmail.com>
2025-07-12 13:48:35 +0545

    mock usecase

---
+++ test/mocks/usecases_mocks.dart
@@ -0,0 +1,6 @@
+import 'package:mockito/annotations.dart';
+import 'package:petforpat/features/auth/domain/usecases/register_usecase.dart';
+import 'package:petforpat/features/auth/domain/usecases/login_usecase.dart';
+
+@GenerateMocks([RegisterUseCase, LoginUseCase])
+void main() {}
